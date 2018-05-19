6e2e380d8c9ea48ea01ab1549c2b98f518e2bd5f
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-05-19 11:59:08 +0200

    Fix bug with event listeners

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@
   function showResult() {
     result.innerHTML = roundLimit + ' round game' + '<br>' + 'YOU ' + playerWins + ' : ' + aiWins + ' AI';
   }
-  showResult();
+  // showResult();
 
   // game off function
   var gameOverMsg = ' game over, please press the new game button!';
@@ -34,7 +34,6 @@
 
   var playerMove = function (move) {
 
-
     var rounds = roundLimit;
     var ai = function () {
       var aiPick = ['paper', 'rock', 'scissors'];
@@ -47,9 +46,11 @@
     } else if ((move == 'paper') && (aiMove == 'rock') || (move == 'rock') && (aiMove == 'scissors') || (move == 'scissors') && (aiMove == 'paper')) {
       output.innerText = 'YOU WON: you played ' + move.toUpperCase() + ' and computer played ' + aiMove.toUpperCase();
       playerWins++;
+      console.log('PLAYER: ' + playerWins);
     } else {
       output.innerText = 'Computer WON: you played ' + move.toUpperCase() + ' but computer played: ' + aiMove.toUpperCase();
       aiWins++;
+      console.log('AI: ' + aiWins);
     }
 
     //entire game wins
@@ -65,23 +66,26 @@
     }
   }
 
-
+  function gameStart() {
+    btnPaper.addEventListener('click', function () {
+      playerMove('paper');
+    });
+    btnRock.addEventListener('click', function () {
+      playerMove('rock');
+    });
+    btnScissors.addEventListener('click', function () {
+      playerMove('scissors');
+    });
+  }
   // listeners
-  btnPaper.addEventListener('click', function () {
-    playerMove('paper');
-  });
-  btnRock.addEventListener('click', function () {
-    playerMove('rock');
-  });
-  btnScissors.addEventListener('click', function () {
-    playerMove('scissors');
-  });
+
   btnNewGame.addEventListener('click', function () {
     roundLimit = roundCounter();
     playerWins = 0;
     aiWins = 0;
     output.innerHTML = '';
     showResult();
+    gameStart();
   });
 
 })();
