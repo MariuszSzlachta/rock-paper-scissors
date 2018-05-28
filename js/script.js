3a0fd5c46a0fb467e10ffc22ae38418c5ff7c066
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-05-28 21:07:59 +0200

    Complete stage 9.5

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ var playerMoveElements = document.querySelectorAll('.player-move'),
   btnNewGame = document.querySelector('.newGame'),
   buttons = document.querySelectorAll('.btn'),
   output = document.querySelector('#output'),
-  result = document.querySelector('#result');
+  result = document.querySelector('#result'),
+  tableBody = document.querySelector('.table-body');
 // ---------------EVENT LISTENERS------------------
 btnClose.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -21,7 +22,9 @@ var params = {
   aiWins: 0,
   roundLimit: 0,
   gameOverMsg: ' game over, please press the new game button!',
-  gamePossible: false
+  gamePossible: false,
+  progress: [],
+  tableRow: []
 }
 // prompt
 var roundCounter = function () {
@@ -32,14 +35,13 @@ function showResult() {
   result.innerHTML = params.roundLimit + ' round game' + '<br>' + 'YOU ' + params.playerWins + ' : ' + params.aiWins + ' AI';
 }
 
-
-  for (var i = 0; i < playerMoveElementsLen; i++) {
-    playerMoveElements[i].addEventListener('click', function () {
-      if (params.gamePossible === true){
-        game(this.getAttribute('data-move'));
-      }
-    });
-  }
+for (var i = 0; i < playerMoveElementsLen; i++) {
+  playerMoveElements[i].addEventListener('click', function () {
+    if (params.gamePossible === true) {
+      game(this.getAttribute('data-move'));
+    }
+  });
+}
 
 // reset parametrów
 function resetParams() {
@@ -49,12 +51,22 @@ function resetParams() {
   output.classList.remove('hide');
   output.innerHTML = '';
   params.gamePossible = true;
+  params.tableRow = [];
+  params.progress = [];
+  tableBody.innerHTML = '';
 }
 
 function gameStart() {
   resetParams();
   showResult();
   delPropagation(); // czy trzeba to wu wywołać? Czy można deklaracje zamienić na IIFE ?
+}
+
+function showEndGameTable() {
+  for (var i = 0; i < params.progress.length; i++) {
+    params.tableRow.push('<tr><td>' + params.progress[i].rounds + '</td><td>' + params.progress[i].plMove + '</td><td>' + params.progress[i].aiMove + '</td><td>' + params.progress[i].roundResult + '</td><td>' + params.progress[i].gameResult + '</td></tr>');
+    tableBody.innerHTML += params.tableRow[i];
+  }
 }
 
 function gameOver(e) {
@@ -82,26 +94,40 @@ function game(playerMove) {
   //remis
   if (playerMove == aiMove) {
     output.innerText = 'It\'s TIE! Player and computer played ' + playerMove.toUpperCase();
+    roundResult = '0 : 0';
     //player wins
   } else if ((playerMove == 'paper') && (aiMove == 'rock') || (playerMove == 'rock') && (aiMove == 'scissors') || (playerMove == 'scissors') && (aiMove == 'paper')) {
     output.innerText = 'YOU WON: you played ' + playerMove.toUpperCase() + ' and computer played ' + aiMove.toUpperCase();
     params.playerWins++;
+    roundResult = '1 : 0';
     // ai wins
   } else {
     output.innerText = 'Computer WON: you played ' + playerMove.toUpperCase() + ' but computer played: ' + aiMove.toUpperCase();
     params.aiWins++;
+    roundResult = '0 : 1';
   }
-//-------------------------- game----------------
+  // tworzenie obiektu co kolejkę
+  params.progress.push({
+    rounds: params.roundLimit,
+    plMove: playerMove,
+    aiMove: aiMove,
+    roundResult: roundResult,
+    gameResult: params.playerWins + ' : ' + params.aiWins
+  });
+  //-------------------------- game----------------
   showResult();
   // warunki wygranej całej gry
   if (params.playerWins == params.roundLimit) {
     comunicatField.innerHTML = 'YOU WON THE ENTIRE GAME ' + '<br>' + params.gameOverMsg;
     gameOver();
+    showEndGameTable();
   }
   if (params.aiWins == params.roundLimit) {
     comunicatField.innerHTML = 'AI WON THE ENTIRE GAME ' + '<br>' + params.gameOverMsg;
     gameOver();
+    showEndGameTable();
   }
+
 };
 
 // -----------------------------obsługa modali----------------------------------------
